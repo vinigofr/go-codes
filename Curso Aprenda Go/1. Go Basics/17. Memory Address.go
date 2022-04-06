2569f1813f477c4cf6779c9091f81e043008d18c
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-04-06 14:13:53 -0300

    Adiciona conteúdo sobre ponteiros

---
+++ Curso Aprenda Go/1. Go Basics/17. Memory Address.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Vinicius"
+
+	fmt.Println("Referenciando nome", &name, "<- Local em memória")
+
+	// Pegamos o &name, que é o local de memória e alocamos ao 
+	var name2 *string = &name 
+	fmt.Println("Desreferenciando nome", *name2, "<- Valor")
+
+
+}
