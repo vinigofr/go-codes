9a0f05982626bd65bf26714a3d9c28aadd4cf281
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-05-03 11:50:18 -0300

    Adiciona exercício 1 que incentiva a criação de uma documentação e representar seu uso no godoc localhost

---
+++ Curso Aprenda Go/0. Exercícios/Level 12/main.go
@@ -0,0 +1,9 @@
+// Aí, aí, aí, ui, ui, ui, AU, AU
+package cachorro
+
+/*
+	Essa função converte a idade de humanos para doguinhos
+*/
+func Idade(anos int) int {
+	return anos * 7
+}
--- Go_docs/package/main.go
+++
@@ -1,8 +0,0 @@
-package oi
-
-import "fmt"
-
-// Essa função diz "Oi" ao ser chamada
-func Oi() {
-	fmt.Println("Oi")
-}
