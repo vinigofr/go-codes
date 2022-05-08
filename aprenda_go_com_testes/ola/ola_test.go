aed95bc9b055c05915ba7e4a7c01661b60d9afa7
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-05-08 17:20:41 -0300

    Adiciona comentários didáticos

--- aprenda_go_com_testes/ola/ola_test.go
+++ aprenda_go_com_testes/ola/ola_test.go
@@ -4,6 +4,14 @@ package main
 // Os testes precisam estar em um arquivo chamado xxx_test.go
 // A função de teste precisa começar com "Test", func TestGo () {}
 // A função de testes recebe um argumento, que é t *testing.T
+
+/*
+   Alguns passos importantes:
+    1. Podemos executar uma série de testes através do comando t.Run()
+    2. Podemos utilizar funções auxiliares para nos ajudar a diminuir a
+        quantidade de linhas nos nossos testes. Um exemplo é a função
+        'verificaMensagemCorreta'
+*/
 import "testing"
 
 func TestOla(t *testing.T) {
