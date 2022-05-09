ff23d67fc455e48db8f13b12cfba9df883680d3d
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-05-09 15:58:19 -0300

    Nova tentativa

---
+++ Exercise_github_doc/ghvinicius/main.go
@@ -0,0 +1,12 @@
+package ghvinicius
+
+import "fmt"
+
+func main() {
+	GitHub()
+}
+
+// Essa função retorna uma resposta de sucesso caso tenha sido upada corretamente no github
+func GitHub() {
+	fmt.Println("Documentação enviada com sucesso")
+}
